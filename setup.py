1d3da10a6312c76e0dd5e8737bfc197313072bad
Florian Hammer <florian.hammer@ost.ch>
2022-04-29 16:55:10 +0200

    Added pyOF package currently containing postpro and io modules

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='0.0.0',
+    author='Wind Energy Group - IET - OST Rapperswil',
+    author_email='max.mustermann@ost.ch',
+    url='http://',
+    packages=find_packages(),
+    license='LICENSE',
+    description='Package description',
+    long_description=open('README.md').read(),
+    install_requires=[""],
+)
